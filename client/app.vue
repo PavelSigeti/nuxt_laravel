906b9dde3776ab00925db7ab80198192a644aa16
PavelSigeti <sigeti352@gmail.com>
2022-12-27 14:03:29 +0300

    init

---
+++ client/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <ul>
+      <li><NuxtLink to="/home">Home</NuxtLink></li>
+      <li><NuxtLink to="/about">About</NuxtLink></li>
+    </ul>
+    <NuxtPage />
+  </div>
+</template>
---
+++ client/pages/home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Just test</h1>
+  </div>
+</template>
